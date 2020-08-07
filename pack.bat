5628b054eb9d60a19aa9b052f3909b6d9c2e6f23
Kendall Bennett <kendallb@amain.com>
2020-08-07 12:49:52 -0700

    Added symbol package

--- pack.bat
+++ pack.bat
@@ -1,3 +1,3 @@
 @echo off
 rmdir /S /Q NuGet
-src\.nuget\NuGet pack src\ShipEngineApi -Symbols -Properties Configuration=Release -OutputDirectory NuGet
+src\.nuget\NuGet pack src\ShipEngineApi -Symbols -SymbolPackageFormat snupkg -Properties Configuration=Release -OutputDirectory NuGet
