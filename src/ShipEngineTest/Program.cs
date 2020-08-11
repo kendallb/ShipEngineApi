87a7bf786d417b0c9ca03e534ce3c4566e959c59
Kendall Bennett <kendallb@amain.com>
2020-08-11 16:35:20 -0700

    More updates to fix nullable entries and things the API responses are returning that fail.

--- src/ShipEngineTest/Program.cs
+++ src/ShipEngineTest/Program.cs
@@ -4,6 +4,8 @@
  */
 
 using System;
+using System.Collections.Generic;
+using System.Linq;
 using System.Threading.Tasks;
 using Newtonsoft.Json;
 using ShipEngineAPI;
@@ -15,23 +17,20 @@ namespace ShipEngineTest
     {
         public static async Task Main()
         {
-            const string toParse = "424 Otterson Drive, Chico CA 95928";
-            Console.WriteLine($"Parsing: {toParse}");
-
-            try
-            {
+            try {
                 using var httpClient = new SingleInstanceHttpClient();
                 var client = new ShipEngineClient(httpClient, "TEST_UviO3xlrk2l3tdqyTLUZ9flQuDtVwRBTUlVZgZmCQA4");
-                var response = await client.ParseAddress(new ParseAddressRequestBody
-                {
+                client.ReadResponseAsString = true;
+                const string toParse = "424 Otterson Drive, Chico CA 95928";
+                Console.WriteLine($"Parsing: {toParse}");
+                var response = await client.ParseAddress(new ParseAddressRequestBody {
                     Text = toParse,
                 });
                 Console.WriteLine(JsonConvert.SerializeObject(response, Formatting.Indented));
 
                 // Now try to validate the address
-                var validated = await client.ValidateAddress(new [] {
-                    new AddressToValidate
-                    {
+                var validated = await client.ValidateAddress(new[] {
+                    new AddressToValidate {
                         AddressLine1 = "100 Unknown Street",
                         CityLocality = "Chico",
                         StateProvince = "CA",
@@ -40,10 +39,8 @@ namespace ShipEngineTest
                     },
                 });
                 Console.WriteLine(JsonConvert.SerializeObject(validated, Formatting.Indented));
-
-                validated = await client.ValidateAddress(new [] {
-                    new AddressToValidate
-                    {
+                validated = await client.ValidateAddress(new[] {
+                    new AddressToValidate {
                         AddressLine1 = "424 Otterson Drive",
                         CityLocality = "Chico",
                         StateProvince = "CA",
@@ -53,15 +50,56 @@ namespace ShipEngineTest
                 });
                 Console.WriteLine(JsonConvert.SerializeObject(validated, Formatting.Indented));
 
+                // Get a list of available carriers
                 var carriers = await client.ListCarriers();
                 Console.WriteLine(JsonConvert.SerializeObject(carriers, Formatting.Indented));
-            }
-            catch (Exception e)
-            {
+
+                // Try to rate a package
+                var rates = await client.CalculateRates(new CalculateRatesRequestBody {
+                    Shipment = new AddressValidatingShipment {
+                        ValidateAddress = ValidateAddress.NoValidation,
+                        ShipDate = DateTimeOffset.Now.Date,
+                        ShipTo = new Address {
+                            Name = "Joe Blogs",
+                            Phone = "530-894-0797",
+                            AddressLine1 = "411 Otterson Drive",
+                            CityLocality = "Chico",
+                            StateProvince = "CA",
+                            PostalCode = "95928",
+                            CountryCode = "US",
+                        },
+                        ShipFrom = new Address {
+                            Name = "Online Seller",
+                            Phone = "530-894-0797",
+                            AddressLine1 = "424 Otterson Drive",
+                            CityLocality = "Chico",
+                            StateProvince = "CA",
+                            PostalCode = "95928",
+                            CountryCode = "US",
+                        },
+                        Packages = new List<Package> {
+                            new Package {
+                                Weight = new Weight {
+                                    Value = 1.0,
+                                    Unit = WeightUnit.Pound,
+                                },
+                                Dimensions = new Dimensions {
+                                    Length = 1,
+                                    Width = 2,
+                                    Height = 3,
+                                },
+                            },
+                        },
+                    },
+                    RateOptions = new RateRequestBody {
+                        CarrierIds = carriers.Carriers.Select(p => p.CarrierId).ToList(),
+                    },
+                });
+                Console.WriteLine(JsonConvert.SerializeObject(rates, Formatting.Indented));
+            } catch (Exception e) {
                 Console.WriteLine("FAILED!");
                 Console.WriteLine(e.Message);
-                if (e.InnerException != null)
-                {
+                if (e.InnerException != null) {
                     Console.WriteLine(e.InnerException.Message);
                 }
             }
